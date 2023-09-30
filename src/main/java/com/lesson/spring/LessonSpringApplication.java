3ad6eab0fcc8398fea86c091d747aeb3995c1f72
bfine <ul8415@gmail.com>
2023-09-30 12:51:33 +0900

    initial commit

---
+++ src/main/java/com/lesson/spring/LessonSpringApplication.java
@@ -0,0 +1,13 @@
+package com.lesson.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LessonSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LessonSpringApplication.class, args);
+    }
+
+}
